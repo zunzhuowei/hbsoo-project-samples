6a51467fdc23cdc25b7ada7793917b9d51620f2d
zunzhuowei <1808862723@qq.com>
2021-08-02 00:39:46 +0800

    server sample

--- hbsoo-server-samples/src/main/java/com/hbsoo/samples/ServerSamplesApplication.java
+++ hbsoo-server-samples/src/main/java/com/hbsoo/samples/ServerSamplesApplication.java
@@ -1,5 +1,6 @@
 package com.hbsoo.samples;
 
+import com.hbsoo.handler.constants.ServerProtocolType;
 import com.hbsoo.server.HbsooServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -14,7 +15,11 @@ public class ServerSamplesApplication {
     public static void main(String[] args) throws InterruptedException {
         final ConfigurableApplicationContext context = SpringApplication.run(ServerSamplesApplication.class, args);
         final HbsooServer hbsooServer = context.getBean(HbsooServer.class);
-        hbsooServer.start();
+        hbsooServer.create(1, 2)
+                .protocolType(ServerProtocolType.HTTP)
+                //.start(5152);
+                .start(7777);
+
     }
 
 
